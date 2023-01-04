4e6a03023a8853e99bd4b71cea4938f2b551c0d8
김형기 <khk211113@naver.com>
2023-01-04 18:14:04 +0900

    feat: 일기 수정 및 받은 일기 목록 api 추가

--- GongGanGam/src/main/java/site/gonggangam/gonggangam_server/controller/DiaryController.java
+++ GongGanGam/src/main/java/site/gonggangam/gonggangam_server/controller/DiaryController.java
@@ -15,6 +15,7 @@ import org.springframework.web.multipart.MultipartFile;
 import site.gonggangam.gonggangam_server.dto.diary.CalendarResponseDto;
 import site.gonggangam.gonggangam_server.dto.diary.DiaryRequestDto;
 import site.gonggangam.gonggangam_server.dto.diary.DiaryResponseDto;
+import site.gonggangam.gonggangam_server.dto.diary.SharedDiaryResponseDto;
 
 import java.util.List;
 
@@ -37,9 +38,18 @@ public class DiaryController {
     public ResponseEntity<DiaryResponseDto> postDiary(
             @Parameter(description = "일기에 업로드할 이미지 파일을 multipart/form-data 형식으로 받습니다.", content = @Content(mediaType = MediaType.MULTIPART_FORM_DATA_VALUE))
             @RequestParam(value = "imgFile", required = false) MultipartFile imgFile,
-            @Parameter(description = "일기 상세 내용을 application/json 형식으로 입력 받습니다.", content = @Content(schema = @Schema(implementation = DiaryRequestDto.class), mediaType = MediaType.APPLICATION_JSON_VALUE))
-            @ModelAttribute DiaryRequestDto body
+            @Parameter(description = "일기 상세 내용을 application/json 형식으로 입력 받습니다.", content = @Content(schema = @Schema(implementation = DiaryRequestDto.Post.class), mediaType = MediaType.APPLICATION_JSON_VALUE))
+            @ModelAttribute DiaryRequestDto.Post data
             ) {
+        return null;
+    }
+
+    @Operation(summary = "공유받은 일기 목록", description = "다른 사용자에게 공유 받은 일기 목록을 조회합니다.")
+    @GetMapping("/shared")
+    public ResponseEntity<List<SharedDiaryResponseDto>> getSharedDiaries(
+            @RequestParam(value = "page") Integer page,
+            @RequestParam(value = "pageSize") Integer pageSize
+    ) {
         return null;
     }
 
@@ -64,6 +74,17 @@ public class DiaryController {
     public ResponseEntity<List<CalendarResponseDto>> getDiaries(
             @RequestParam Integer year,
             @RequestParam Integer month
+    ) {
+        return null;
+    }
+
+    @Operation(summary = "일기 수정", description = "작성된 일기의 내용을 수정합니다. request body 형식은 application/json을 선택해서 참조해주세요.")
+    @PutMapping(value = "/{diaryId}", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE, MediaType.APPLICATION_JSON_VALUE})
+    public ResponseEntity<DiaryResponseDto> putDiary(
+            @Parameter(description = "일기에 업로드할 이미지 파일을 multipart/form-data 형식으로 받습니다.", content = @Content(mediaType = MediaType.MULTIPART_FORM_DATA_VALUE))
+            @RequestParam(value = "imgFile", required = false) MultipartFile imgFile,
+            @Parameter(description = "일기 상세 내용을 application/json 형식으로 입력 받습니다.", content = @Content(schema = @Schema(implementation = DiaryRequestDto.Put.class), mediaType = MediaType.APPLICATION_JSON_VALUE))
+            @ModelAttribute DiaryRequestDto.Put data
     ) {
         return null;
     }
--- GongGanGam/src/main/java/site/gonggangam/gonggangam_server/dto/diary/DiaryRequestDto.java
+++ GongGanGam/src/main/java/site/gonggangam/gonggangam_server/dto/diary/DiaryRequestDto.java
@@ -7,17 +7,32 @@ import lombok.Data;
 
 import java.time.LocalDate;
 
-@Data
-@Builder
-@Schema(description = "일기 작성")
 public class DiaryRequestDto {
-    @Schema(description = "작성 일자", defaultValue = "2023-01-04", required = true)
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd", timezone = "Asia/Seoul")
-    private final LocalDate date;
-    @Schema(description = "이모지", defaultValue = "happy", required = true)
-    private final String emoji;
-    @Schema(description = "내용", defaultValue = "내 손 잡아준 너 매일 아침 눈을 뜰 때면", required = true)
-    private final String content;
-    @Schema(description = "공유 여부", defaultValue = "true", required = true)
-    private final Boolean shareAgreed;
+
+    @Data
+    @Builder
+    @Schema(description = "일기 작성")
+    public static class Post {
+        @Schema(description = "작성 일자", defaultValue = "2023-01-04", required = true)
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd", timezone = "Asia/Seoul")
+        private final LocalDate date;
+        @Schema(description = "이모지", defaultValue = "happy", required = true)
+        private final String emoji;
+        @Schema(description = "내용", defaultValue = "내 손 잡아준 너 매일 아침 눈을 뜰 때면", required = true)
+        private final String content;
+        @Schema(description = "공유 여부", defaultValue = "true", required = true)
+        private final Boolean shareAgreed;
+    }
+
+    @Data
+    @Builder
+    @Schema(description = "일기 수정")
+    public static class Put {
+        @Schema(description = "이모지", defaultValue = "happy", required = true)
+        private final String emoji;
+        @Schema(description = "내용", defaultValue = "내 손 잡아준 너 매일 아침 눈을 뜰 때면", required = true)
+        private final String content;
+        @Schema(description = "공유 여부", defaultValue = "true", required = true)
+        private final Boolean shareAgreed;
+    }
 }
