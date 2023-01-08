f6873397e634a2e08cda8e9bcd99ac7d9a94eb6d
김형기 <khk211113@naver.com>
2023-01-08 18:06:11 +0900

    fix: ShareDiary PK 필드 수정

--- GongGanGam/src/main/java/site/gonggangam/gonggangam_server/domain/diary/ShareDiaryPK.java
+++ GongGanGam/src/main/java/site/gonggangam/gonggangam_server/domain/diary/ShareDiaryPK.java
@@ -3,6 +3,6 @@ package site.gonggangam.gonggangam_server.domain.diary;
 import java.io.Serializable;
 
 public class ShareDiaryPK implements Serializable {
-    private Long diaryId;
-    private Long receiverId;
+    private Long diary;
+    private Long receiver;
 }
