70e459938d28a5616a5816aa80c0befca4593982
김형기 <khk211113@naver.com>
2023-01-09 23:53:20 +0900

    feat: ChatMessage 엔터티

--- GongGanGam/src/main/java/site/gonggangam/gonggangam_server/domain/chat/ChatMessage.java
+++ GongGanGam/src/main/java/site/gonggangam/gonggangam_server/domain/chat/ChatMessage.java
@@ -1,4 +1,32 @@
 package site.gonggangam.gonggangam_server.domain.chat;
 
-public class ChatMessage {
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import site.gonggangam.gonggangam_server.domain.BaseTimeEntity;
+import site.gonggangam.gonggangam_server.domain.users.Users;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "CHAT_MESSAGE")
+@Entity
+public class ChatMessage extends BaseTimeEntity {
+
+    @Id
+    @Column(name = "MESSAGE_ID")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long messageId;
+
+    @ManyToOne
+    @JoinColumn(name = "CHAT_ROOM_ID", referencedColumnName = "ROOM_ID")
+    private ChatRoom chatRoom;
+
+    @ManyToOne
+    @JoinColumn(name = "SENDER_ID", referencedColumnName = "USER_ID")
+    private Users sender;
+
+    @Column(name = "CONTENT", columnDefinition = "TEXT", nullable = false)
+    private String content;
 }
