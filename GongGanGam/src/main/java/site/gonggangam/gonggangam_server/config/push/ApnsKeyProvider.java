49d1d466394ad6f1d0f6c90ef0eb767e81651e20
김형기 <khk211113@naver.com>
2023-01-27 18:09:01 +0900

    feat: APNs key provider

--- GongGanGam/src/main/java/site/gonggangam/gonggangam_server/config/push/ApnsKeyProvider.java
+++ GongGanGam/src/main/java/site/gonggangam/gonggangam_server/config/push/ApnsKeyProvider.java
@@ -2,15 +2,31 @@ package site.gonggangam.gonggangam_server.config.push;
 
 import com.turo.pushy.apns.auth.ApnsSigningKey;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.stereotype.Component;
 import site.gonggangam.gonggangam_server.config.ResponseCode;
 import site.gonggangam.gonggangam_server.config.exceptions.GeneralException;
 
+import java.io.InputStream;
+
+@Component
 @Slf4j
 public class ApnsKeyProvider {
 
+    @Value("${apns.team-id}")
+    private String teamId;
+
+    @Value("${apns.key.id}")
+    private String keyId;
+
+    @Value("${apns.key.file-name}")
+    private String keyFileName;
+
     public ApnsSigningKey getKey() throws GeneralException {
         try {
-            return new ApnsSigningKey()
+            InputStream pkcs = new ClassPathResource(keyFileName).getInputStream();
+            return ApnsSigningKey.loadFromInputStream(pkcs, teamId, keyId);
         } catch (Exception e) {
             log.error(String.format("[%s] apns signing error : %s", getClass(), e));
             throw new GeneralException(ResponseCode.APNS_SIGNING_ERROR);
