6a0c50b5645239b7ad501b9e92b8148b5ad0e450
김형기 <khk211113@naver.com>
2023-01-30 06:58:18 +0900

    feat: 공평한 일기 공유 배분 처리

--- GongGanGam/src/main/java/site/gonggangam/gonggangam_server/service/ShareDiaryServiceImpl.java
+++ GongGanGam/src/main/java/site/gonggangam/gonggangam_server/service/ShareDiaryServiceImpl.java
@@ -1,5 +1,6 @@
 package site.gonggangam.gonggangam_server.service;
 
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Async;
@@ -7,6 +8,8 @@ import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import site.gonggangam.gonggangam_server.domain.diary.Diary;
 import site.gonggangam.gonggangam_server.domain.diary.ShareDiary;
 import site.gonggangam.gonggangam_server.domain.users.Users;
@@ -16,7 +19,8 @@ import site.gonggangam.gonggangam_server.domain.repository.ShareDiaryRepository;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -63,35 +67,70 @@ public class ShareDiaryServiceImpl implements ShareDiaryService {
     }
 
     /**
-     * 그룹별 셔플하여 공유하되 자신에게 공유되지 않도록 다음과 같은 로직을 이용합니다.
-     * <p>
-     * 각 일기는 자신의 index + <code>{@link #createOffset(int)}</code> 에서 생성된 offset 위치의 일기와 매칭됩니다.
-     * <p>
-     * 만약 그룹 크기가 1이하이면 아무것도 실행하지 않습니다.
+     * 그룹화된 일기들을 공유합니다. <p>
+     * 각 일기에 대해서 그룹 내 다른 일기 작성자에게 공유합니다. <p>
+     * 공유 수신자는 항상 자기 자신이 아닙니다. <p>
+     * 다음의 로직을 사용합니다. <p>
+     * 1) {@link #createOffset(int)}을 이용해 {@code offset}을 생성합니다. <p>
+     * 2) 1개의 일기를 작성한 작성자의 일기는 일기의 ({@code index} + {@code offset}) % {@code size} 위치의 일기의 작성자에게 공유됩니다. <p>
+     * 3) 1개를 초과한 작성자의 나머지 일기는 {@link #createDestIndex(Set, int)} 를 이용해 자신의 일기를 블랙리스트로 추가하여 다른 일기와 매칭됩니다.
      * @see #createOffset(int)
-     * @param diaries 공유하고자 하는 그룹화된 일기 목록
+     * @see #createDestIndex(Set, int)
+     * @param diaries 그룹화된 일기 목록
      */
     private void shareGroupedDiaries(List<Diary> diaries) {
-        int offset;
         final int size = diaries.size();
+        if (size <= 1) return;
 
-        try {
-            offset = createOffset(size);
-            log.info("group offset selected " + offset);
-        } catch (IllegalArgumentException e) {
-            log.info("group size is smaller than 2. diary share terminates.");
-            return;
-        }
+        int offset = createOffset(size);
+        Set<Long> diarySharedUsers = new HashSet<>();
+        MultiValueMap<Long, DiaryWithIdx> packedByWriter = packByWriterId(diaries);
 
-        for (int idx = 0; idx < size; idx++) {
-            Diary dest = diaries.get(idx);
-            Users receiver = diaries.get((idx + offset) % size).getWriter();
-            shareDiary(dest, receiver);
+        for (int idx = 0; idx < diaries.size(); idx++) {
+            Diary diary = diaries.get(idx);
+            Long writerId = diary.getWriter().getUserId();
 
+            Set<Integer> blacklist = packedByWriter.get(writerId)
+                    .stream()
+                    .map(DiaryWithIdx::index)
+                    .collect(Collectors.toSet());
+
+            Users receiver;
+
+            // 아직 한번도 공유를 못한 사용자는 offset 으로 처리
+            if (!diarySharedUsers.contains(writerId)) {
+                receiver = diaries.get((idx + offset) % size).getWriter();
+            }
+            // 한번 공유를 한 사용자는 랜덤 index 생성으로 처리
+            else {
+                int destIdx = createDestIndex(blacklist, size);
+                receiver = diaries.get(destIdx).getWriter();
+            }
+
+            // 일기 공유
+            shareDiary(diary, receiver);
+            diarySharedUsers.add(writerId);
+
+            // 알림 설정 되어 있을 경우에만 푸시알림
             if (receiver.getSettings().getNotifyDiary()) {
                 notifyDiaryShared(receiver);
             }
         }
+    }
+
+    /**
+     * 작성자 id로 Map을 생성합니다.
+     * @param diaries 일기 목록
+     * @return 작성자로 구분된 일기목록
+     */
+    private MultiValueMap<Long, DiaryWithIdx> packByWriterId(List<Diary> diaries) {
+        MultiValueMap<Long, DiaryWithIdx> packedByWriter = new LinkedMultiValueMap<>();
+
+        for (int idx = 0; idx < diaries.size(); idx++) {
+            Diary diary = diaries.get(idx);
+            packedByWriter.add(diary.getWriter().getUserId(), new DiaryWithIdx(diary, idx));
+        }
+        return packedByWriter;
     }
 
     /**
@@ -102,8 +141,8 @@ public class ShareDiaryServiceImpl implements ShareDiaryService {
      * @return size 보다 작게 생성된 offset
      * @throws IllegalStateException 그룹의 크기가 1이하일 때 예외 발생
      */
-    private static int createOffset(final int size) throws IllegalArgumentException {
-        if (size <= 1) throw new IllegalArgumentException();
+    private int createOffset(final int size) throws IllegalArgumentException {
+        if (size <= 1) throw new IllegalArgumentException(String.format("[%s] Diary group size must larger than 1.", getClass()));
         int offset;
 
         do {
@@ -112,6 +151,29 @@ public class ShareDiaryServiceImpl implements ShareDiaryService {
         } while (offset >= size);
 
         return offset;
+    }
+
+
+    /**
+     * 공유할 일기 위치를 생성합니다.
+     * <p>
+     * 반환 값은 0 이상 {@code size} 미만 범위의 정수 중 현재 일기의 위치인 {@code curIdx} 와 다른 것입니다.
+     * @param blacklist 뽑지 않을 일기의 위치 (동일 일기 선택 방지)
+     * @param size 일기 그룹의 크기
+     * @return 0 이상 {@code size} 미만의 정수 중 {@code curIdx} 가 아닌 임의의 정수
+     * @throws IllegalStateException 그룹의 크기가 1이하일 때 예외 발생
+     */
+    private int createDestIndex(final Set<Integer> blacklist, final int size) throws IllegalArgumentException {
+        if (size <= 1) throw new IllegalArgumentException(String.format("[%s] Diary group size must larger than 1.", getClass()));
+
+        Random random = new Random(System.currentTimeMillis());
+        int index;
+
+        do {
+            index = random.nextInt(size);
+        } while (blacklist.contains(index));
+
+        return index;
     }
 
     /**
@@ -146,4 +208,5 @@ public class ShareDiaryServiceImpl implements ShareDiaryService {
 
     }
 
+    private record DiaryWithIdx(Diary diary, Integer index) { }
 }
