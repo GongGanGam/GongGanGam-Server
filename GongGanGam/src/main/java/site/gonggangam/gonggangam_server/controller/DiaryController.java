d027f8eb156926264e81b33db688d2b055b691b5
김형기 <khk211113@naver.com>
2023-02-05 04:09:41 +0900

    feat: DiaryController 적용

--- GongGanGam/src/main/java/site/gonggangam/gonggangam_server/controller/DiaryController.java
+++ GongGanGam/src/main/java/site/gonggangam/gonggangam_server/controller/DiaryController.java
@@ -8,6 +8,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.apache.http.auth.AUTH;
 import org.springdoc.api.annotations.ParameterObject;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -15,6 +16,9 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import site.gonggangam.gonggangam_server.config.HttpServletUtil;
 import site.gonggangam.gonggangam_server.config.ResponseCode;
+import site.gonggangam.gonggangam_server.config.swagger.ApiResponseCode;
+import site.gonggangam.gonggangam_server.config.swagger.ApiResponseCodeGroup;
+import site.gonggangam.gonggangam_server.config.swagger.ApiResponseCodes;
 import site.gonggangam.gonggangam_server.service.dto.DataResponseDto;
 import site.gonggangam.gonggangam_server.service.dto.ErrorResponseDto;
 import site.gonggangam.gonggangam_server.service.dto.SuccessResponseDto;
@@ -35,13 +39,14 @@ public class DiaryController {
     private final DiaryService diaryService;
 
     @Operation(summary = "일기 작성", description = "원하는 날짜에 일기를 작성합니다. (스웨거에서 file 첨부를 하지 않을 때는 'send empty value' 체크를 해제해주세요.)")
-    @ApiResponses(
+    @ApiResponseCodes(
             value = {
-                    @ApiResponse(responseCode = "201", description = "작성 성공"),
-                    @ApiResponse(responseCode = "401", description = "인증에 실패했습니다.", content = @Content(schema = @Schema(implementation = ErrorResponseDto.class))),
-                    @ApiResponse(responseCode = "403", description = "만료된 토큰입니다.", content = @Content(schema = @Schema(implementation = ErrorResponseDto.class))),
-                    @ApiResponse(responseCode = "409", description = "이미 일기가 작성된 날짜입니다.", content = @Content(schema = @Schema(implementation = ErrorResponseDto.class))),
-                    @ApiResponse(responseCode = "409", description = "오늘 이후 날짜에 작성할 수 없습니다.", content = @Content(schema = @Schema(implementation = ErrorResponseDto.class)))
+                    @ApiResponseCode(ResponseCode.CREATED),
+                    @ApiResponseCode(ResponseCode.DIARY_DATE_CONFLICT),
+                    @ApiResponseCode(ResponseCode.DIARY_DATE_INVALID)
+            },
+            groups = {
+                    ApiResponseCodeGroup.AUTHENTICATED
             }
     )
     @PostMapping(consumes = {MediaType.MULTIPART_FORM_DATA_VALUE, MediaType.APPLICATION_JSON_VALUE}, produces = {MediaType.APPLICATION_JSON_VALUE})
@@ -57,6 +62,14 @@ public class DiaryController {
     }
 
     @Operation(summary = "공유받은 일기 목록", description = "다른 사용자에게 공유 받은 일기 목록을 조회합니다.")
+    @ApiResponseCodes(
+            value = {
+                    @ApiResponseCode(ResponseCode.OK)
+            },
+            groups = {
+                    ApiResponseCodeGroup.AUTHENTICATED
+            }
+    )
     @GetMapping("/shared")
     public DataResponseDto<Page<SharedDiaryResponseDto>> getSharedDiaries(
             HttpServletRequest request,
@@ -69,12 +82,13 @@ public class DiaryController {
     }
 
     @Operation(summary = "나의 일기 상세조회", description = "일기의 내용을 상세조회합니다.")
-    @ApiResponses(
+    @ApiResponseCodes(
             value = {
-                    @ApiResponse(responseCode = "200", description = "조회 성공"),
-                    @ApiResponse(responseCode = "401", description = "인증에 실패했습니다.", content = @Content(schema = @Schema(implementation = ErrorResponseDto.class))),
-                    @ApiResponse(responseCode = "403", description = "만료된 토큰입니다.", content = @Content(schema = @Schema(implementation = ErrorResponseDto.class))),
-                    @ApiResponse(responseCode = "404", description = "올바르지 않은 경로입니다.", content = @Content(schema = @Schema(implementation = ErrorResponseDto.class)))
+                    @ApiResponseCode(ResponseCode.OK)
+            },
+            groups = {
+                    ApiResponseCodeGroup.OPTIONAL,
+                    ApiResponseCodeGroup.AUTHENTICATED
             }
     )
     @GetMapping("/{diaryId}")
@@ -85,6 +99,15 @@ public class DiaryController {
     }
 
     @Operation(summary = "캘린더에서 일기 목록 조회하기", description = "연도, 월에 해당하는 일기 목록을 조회합니다.")
+    @ApiResponseCodes(
+            value = {
+                    @ApiResponseCode(ResponseCode.OK),
+                    @ApiResponseCode(ResponseCode.BAD_REQUEST)
+            },
+            groups = {
+                    ApiResponseCodeGroup.AUTHENTICATED
+            }
+    )
     @GetMapping
     public DataResponseDto<CalendarResponseDto> getDiaries(
             HttpServletRequest request,
@@ -97,6 +120,15 @@ public class DiaryController {
     }
 
     @Operation(summary = "일기 수정", description = "작성된 일기의 내용을 수정합니다. (스웨거에서 file 첨부를 하지 않을 때는 'send empty value' 체크를 해제해주세요.)")
+    @ApiResponseCodes(
+            value = {
+                    @ApiResponseCode(ResponseCode.OK)
+            },
+            groups = {
+                    ApiResponseCodeGroup.OPTIONAL,
+                    ApiResponseCodeGroup.AUTHENTICATED
+            }
+    )
     @PutMapping(value = "/{diaryId}", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE, MediaType.APPLICATION_JSON_VALUE})
     public DataResponseDto<DiaryResponseDto> putDiary(
             @PathVariable("diaryId") Long diaryId,
@@ -107,13 +139,13 @@ public class DiaryController {
     }
 
     @Operation(summary = "일기 삭제", description = "일기를 삭제합니다.")
-    @ApiResponses(
+    @ApiResponseCodes(
             value = {
-                    @ApiResponse(responseCode = "201", description = "삭제 성공"),
-                    @ApiResponse(responseCode = "401", description = "인증에 실패했습니다.", content = @Content(schema = @Schema(implementation = ErrorResponseDto.class))),
-                    @ApiResponse(responseCode = "403", description = "권한이 없습니다.", content = @Content(schema = @Schema(implementation = ErrorResponseDto.class))),
-                    @ApiResponse(responseCode = "403", description = "만료된 토큰입니다.", content = @Content(schema = @Schema(implementation = ErrorResponseDto.class))),
-                    @ApiResponse(responseCode = "404", description = "올바르지 않은 경로입니다.", content = @Content(schema = @Schema(implementation = ErrorResponseDto.class)))
+                    @ApiResponseCode(ResponseCode.OK)
+            },
+            groups = {
+                    ApiResponseCodeGroup.OPTIONAL,
+                    ApiResponseCodeGroup.AUTHENTICATED
             }
     )
     @DeleteMapping("/{diaryId}")
