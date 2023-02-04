043f4d7750937c8f6d040ffcdbf2904b78752cd0
김형기 <khk211113@naver.com>
2023-02-05 04:02:54 +0900

    feat: AdminController 적용 완료

--- GongGanGam/src/main/java/site/gonggangam/gonggangam_server/controller/AdminController.java
+++ GongGanGam/src/main/java/site/gonggangam/gonggangam_server/controller/AdminController.java
@@ -1,10 +1,8 @@
 package site.gonggangam.gonggangam_server.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
@@ -15,7 +13,6 @@ import site.gonggangam.gonggangam_server.config.swagger.ApiResponseCode;
 import site.gonggangam.gonggangam_server.config.swagger.ApiResponseCodeGroup;
 import site.gonggangam.gonggangam_server.config.swagger.ApiResponseCodes;
 import site.gonggangam.gonggangam_server.service.dto.DataResponseDto;
-import site.gonggangam.gonggangam_server.service.dto.ErrorResponseDto;
 import site.gonggangam.gonggangam_server.service.dto.ResponseDto;
 import site.gonggangam.gonggangam_server.service.dto.SuccessResponseDto;
 import site.gonggangam.gonggangam_server.service.dto.notice.NoticeRequestDto;
@@ -38,19 +35,13 @@ public class AdminController {
     private final ReportService reportService;
 
     @Operation(summary = "공지사항 작성", description = "관리자 계정만 이용 가능합니다.")
-    @ApiResponses(
-            value = {
-                    @ApiResponse(responseCode = "201", description = "작성 성공"),
-                    @ApiResponse(responseCode = "401", description = "인증에 실패했습니다.", content = @Content(schema = @Schema(implementation = ErrorResponseDto.class))),
-                    @ApiResponse(responseCode = "403", description = "권한이 없습니다.", content = @Content(schema = @Schema(implementation = ErrorResponseDto.class))),
-                    @ApiResponse(responseCode = "403", description = "만료된 토큰입니다.", content = @Content(schema = @Schema(implementation = ErrorResponseDto.class)))
-            }
-    )
     @ApiResponseCodes(
             value = {
-                    @ApiResponseCode(ResponseCode.CREATED)},
+                    @ApiResponseCode(ResponseCode.CREATED)
+            },
             groups = {
-                    ApiResponseCodeGroup.AUTHENTICATED
+                    ApiResponseCodeGroup.AUTHENTICATED,
+                    ApiResponseCodeGroup.PERMITTED
             }
     )
     @PostMapping("/notice")
@@ -66,10 +57,16 @@ public class AdminController {
 
     @Operation(summary = "공지사항 수정", description = "관리자 계정만 이용 가능합니다.")
     @PutMapping("/notice/{noticeId}")
-    @ApiResponseCodes(value = {
-            @ApiResponseCode(ResponseCode.OK),
-            @ApiResponseCode(ResponseCode.TOKEN_EXPIRED)
-    })
+    @ApiResponseCodes(
+            value = {
+                    @ApiResponseCode(ResponseCode.OK)
+            },
+            groups = {
+                    ApiResponseCodeGroup.OPTIONAL,
+                    ApiResponseCodeGroup.AUTHENTICATED,
+                    ApiResponseCodeGroup.PERMITTED
+            }
+    )
     public DataResponseDto<NoticeResponseDto> putNotice(
             @PathVariable("noticeId") Long noticeId,
             @RequestBody NoticeRequestDto.PutNotice body
@@ -79,6 +76,15 @@ public class AdminController {
 
     @Operation(summary = "신고 내역 조회", description = "관리자 계정만 이용 가능. 조회할 신고 타입 type = { all, diary, reply, chat }")
     @GetMapping("/report")
+    @ApiResponseCodes(
+            value = {
+                    @ApiResponseCode(ResponseCode.OK)
+            },
+            groups = {
+                    ApiResponseCodeGroup.AUTHENTICATED,
+                    ApiResponseCodeGroup.PERMITTED
+            }
+    )
     public DataResponseDto<List<ReportResponseDto>> getReport(
             @Schema(defaultValue = "diary")
             @RequestParam("type") String type
@@ -88,9 +94,15 @@ public class AdminController {
 
     @Operation(summary = "신고 처리상태 변경", description = "신고 내역의 처리 상태를 변경합니다. 관리자 계정만 이용 가능합니다. 처리 상태 progress = { before, processing, completed }")
     @PutMapping("/report/{reportId}")
-    @ApiResponseCodes(value = {
-            @ApiResponseCode(ResponseCode.OK)
-    })
+    @ApiResponseCodes(
+            value = {
+                    @ApiResponseCode(ResponseCode.OK)
+            },
+            groups = {
+                    ApiResponseCodeGroup.AUTHENTICATED,
+                    ApiResponseCodeGroup.PERMITTED
+            }
+    )
     public ResponseDto putReport(
             @RequestBody ReportRequestDto.PutReport body
     ) {
